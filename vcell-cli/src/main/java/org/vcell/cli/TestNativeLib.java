fe1004a157b58cd73ef85c1cc91340f2a709b2c8
Jim Schaff <schaff.jim@gmail.com>
2022-08-11 16:40:23 -0400

    System.loadLibrary() with -Djava.library.path=<vcell>/nativelibs/mac64

--- vcell-cli/src/main/java/org/vcell/cli/TestNativeLib.java
+++ vcell-cli/src/main/java/org/vcell/cli/TestNativeLib.java
@@ -1,8 +1,5 @@
 package org.vcell.cli;
 
-import cbit.vcell.resource.NativeLib;
-import cbit.vcell.resource.PropertyLoader;
-import cbit.vcell.resource.ResourceUtil;
 import org.sbml.libcombine.CombineArchive;
 
 public class TestNativeLib {
@@ -10,10 +7,12 @@ public class TestNativeLib {
     public static void main(String[] args) {
         try {
             System.out.println("starting to load combine library");
-            System.setProperty("vcell.installDir", "/Users/schaff/Documents/workspace/vcell");
-            PropertyLoader.loadProperties();
-            ResourceUtil.setNativeLibraryDirectory();
-            NativeLib.combinej.load();
+            try {
+                System.loadLibrary("combinej");
+            }catch (UnsatisfiedLinkError e){
+                throw new RuntimeException("failed to load lib: "+e.getMessage()+" : e.g. -Djava.library.path=/Users/schaff/Documents/workspace/vcell/nativelibs/mac64", e);
+            }
+            // test that the library was loaded.
             CombineArchive combineArchive = new CombineArchive();
             System.out.println("newly created combine archive "+combineArchive);
             System.out.println("completed combine library");
